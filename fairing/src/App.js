8931b69ce0cce9647180107e7db6cdfa6dbe0b33
feger <marc.feger@uni-duesseldorf.de>
2020-12-29 13:11:26 +0100

    Add MenuBar Component

--- fairing/src/App.js
+++ fairing/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import MainPage from "./pages/main/MainPage";
+import MenuBar from "./components/MenuBar";
 
 class App extends Component {
     /**
@@ -10,6 +11,7 @@ class App extends Component {
     render() {
         return (
             <div>
+                <MenuBar/>
                 <MainPage/>
             </div>
         );
---
+++ fairing/src/components/MenuBar.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import {Dropdown, Grid, Image, Menu, Search} from "semantic-ui-react";
+import logo from '../resources/logo.jpg'
+
+class MenuBar extends Component {
+    render() {
+        this.items = ['Account', 'Logout', 'SignUp']
+        return (
+            <Menu secondary fixed={"top"}
+                  inverted={true}
+                  size='large'
+                  style={{background: "#1b1c1d"}}
+            >
+                <Menu.Item name="Logo">
+                    <Image avatar src={logo} alt="Logo"/>
+                </Menu.Item>
+                <Grid>
+                    <Grid.Row only="computer tablet">
+                        {
+                            this.items.map((item, index) =>
+                                <Menu.Item name={item}/>
+                            )
+                        }
+                    </Grid.Row>
+                    <Grid.Row only="mobile">
+                        <Dropdown item icon='bars' simple>
+                            <Dropdown.Menu>
+                                {
+                                    this.items.splice(0, this.items.length - 1).map((item, index) =>
+                                        <Dropdown.Item text={item}/>
+                                    )
+                                }
+                                <Dropdown.Divider/>
+                                <Dropdown.Item text={this.items.pop()}/>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Grid.Row>
+                </Grid>
+                <Menu.Item position='right'>
+                    <Search/>
+                </Menu.Item>
+            </Menu>
+        );
+    }
+}
+
+export default MenuBar;
