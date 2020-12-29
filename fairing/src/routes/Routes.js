0dee07796a1b26a38df7b58e90a8ada95681907f
feger <marc.feger@uni-duesseldorf.de>
2020-12-29 14:45:58 +0100

    Add more routes as they are used in MenuBar

--- fairing/src/routes/Routes.js
+++ fairing/src/routes/Routes.js
@@ -2,10 +2,14 @@ import React from "react";
 import {Route, Switch} from "react-router-dom";
 import MainPage from "../pages/main/MainPage";
 import AccountPage from "../pages/account/AccountPage";
+import LoginPage from "../pages/login/LoginPage";
+import SignUpPage from "../pages/signup/SignUpPage";
 
 export function Routes() {
     return <Switch>
         <Route path="/" exact component={MainPage}/>
         <Route path="/account" exact component={AccountPage}/>
+        <Route path="/logout" exact component={LoginPage}/>
+        <Route path="/signup" exact component={SignUpPage}/>
     </Switch>
 }
