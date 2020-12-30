d5d68de02c3a9534a65164aa0037ae301421dd98
feger <marc.feger@uni-duesseldorf.de>
2020-12-30 14:00:21 +0100

    Add Redirect to MainPage for Login

--- fairing/src/pages/login/LoginForm.js
+++ fairing/src/pages/login/LoginForm.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
-import {Form, Grid, Header, Image, Message, Segment} from "semantic-ui-react";
+import {Form, Grid, Image, Message, Segment} from "semantic-ui-react";
 import logo from '../../resources/logo.jpg'
+import {Redirect} from "react-router-dom";
 
 class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            success: false
+        }
+    }
+
     render() {
+        if (this.state.success)
+            return <Redirect to={{pathname: '/'}}/>
         return (
             <div>
                 <Grid textAlign='center' style={{height: '100vh'}} verticalAlign='middle'>
                     <Grid.Column style={{maxWidth: "70%"}}>
-                        <Image style={{maxWidth: "70%", marginBottom:"3em"}} src={logo} circular size="small" centered/>
+                        <Image style={{maxWidth: "70%", marginBottom: "3em"}}
+                               src={logo}
+                               circular
+                               size="small"
+                               centered/>
                         <Form size='large'>
                             <Segment raised>
                                 <Form.Input
@@ -25,7 +39,12 @@ class LoginForm extends Component {
                                     placeholder='Password'
                                     type='password'
                                 />
-                                <Form.Button type='submit' secondary fluid size='large'>
+                                <Form.Button
+                                    type='submit'
+                                    secondary
+                                    fluid
+                                    size='large'
+                                    onClick={() => this.setState({success: true})}>
                                     Login
                                 </Form.Button>
                             </Segment>
