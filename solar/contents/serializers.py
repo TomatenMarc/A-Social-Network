44112280a9db07903a8c8412fa2c7d75941be77e
feger <marc.feger@uni-duesseldorf.de>
2021-01-04 19:28:05 +0100

    Add hashtag serializer while serializing statements

--- solar/contents/serializers.py
+++ solar/contents/serializers.py
@@ -1,6 +1,16 @@
 from rest_framework import serializers
 
-from .models import Statement
+from .models import Statement, Hashtag
+
+
+class HashtagSerializer(serializers.ModelSerializer):
+    """
+    This serializer can be used to serialize hashtags.
+    """
+
+    class Meta:
+        model = Hashtag
+        fields = ("id", "tag",)
 
 
 class StatementSerializer(serializers.ModelSerializer):
@@ -9,6 +19,8 @@ class StatementSerializer(serializers.ModelSerializer):
     It can be used to serialize content of a statement.
     """
 
+    tagged = serializers.ListField(source='get_hashtags', child=HashtagSerializer())
+
     class Meta:
         model = Statement
-        fields = ('content',)
+        fields = ('content', 'tagged',)
