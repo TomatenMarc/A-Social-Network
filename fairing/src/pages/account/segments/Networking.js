e953e6ea50b158558272d36bd7deab152fa52135
feger <marc.feger@uni-duesseldorf.de>
2021-01-09 23:50:35 +0100

    Comment Networking.js

--- fairing/src/pages/account/segments/Networking.js
+++ fairing/src/pages/account/segments/Networking.js
@@ -2,8 +2,28 @@ import React, {Component} from 'react';
 import {Grid, Segment} from "semantic-ui-react";
 import Follower from "../components/Follower";
 import Following from "../components/Following";
+import {PropTypes} from "prop-types";
 
 class Networking extends Component {
+
+    /**
+     * This component is for the representation of the network embedding of the user.
+     * It wil distinguish between public and private accounts.
+     * A public account will show less information regarding the network embedding of the specific account.
+     * Todo: Add a modal for showing the friends.
+     * Todo: It should be possible to delete friendships.
+     * @type {{private: *, follower: *, following: *}}
+     */
+    static propTypes = {
+        private: PropTypes.bool.isRequired,
+        following: PropTypes.array.isRequired,
+        follower: PropTypes.array.isRequired
+    };
+
+    /**
+     * This will show the network embedding of the corresponding account.
+     * @returns {JSX.Element}
+     */
     render() {
         if (this.props.private)
             return <Segment style={{padding: '0em'}} vertical>
