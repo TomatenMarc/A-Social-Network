7cf656ebeaf148f77702a2b404f4a5ff511c96ad
feger <marc.feger@uni-duesseldorf.de>
2021-01-10 21:17:30 +0100

    Comment Following.js

--- fairing/src/pages/account/components/Following.js
+++ fairing/src/pages/account/components/Following.js
@@ -1,7 +1,22 @@
 import React, {Component} from 'react';
 import {Header, Icon} from "semantic-ui-react";
+import {PropTypes} from "prop-types";
 
 class Following extends Component {
+    /**
+     * This component shows the account followed by the calling account.
+     * Todo: Add a onClick event which then shows the follows in a model.
+     * It requires the list of following in the props.
+     * @type {{following: *}}
+     */
+    static propTypes = {
+        following: PropTypes.array.isRequired
+    };
+
+    /**
+     * This will show the following accounts.
+     * @returns {JSX.Element}
+     */
     render() {
         return (
             <div>
