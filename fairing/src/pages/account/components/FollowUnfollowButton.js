ee5ff845d2893f6cfb08942014471f6602d0358d
feger <marc.feger@uni-duesseldorf.de>
2021-01-10 21:24:17 +0100

    Comment FollowUnfollowButton.js.js

--- fairing/src/pages/account/components/FollowUnfollowButton.js
+++ fairing/src/pages/account/components/FollowUnfollowButton.js
@@ -1,25 +1,48 @@
 import React, {Component} from 'react';
 import {Button, Icon} from "semantic-ui-react";
-import {withCookies} from "react-cookie";
+import {Cookies, withCookies} from "react-cookie";
 import axios from "axios";
+import {instanceOf, PropTypes} from "prop-types";
 
 class FollowUnfollowButton extends Component {
 
+    /**
+     * This component is a follow/unfollow button.
+     * It needs the information if the corresponding account is a friend of the calling account.
+     * Depending on friend it will decide to make follow or to unfollow.
+     * Therefore it needs the user token for validation.
+     * @type {{uid: *, friend: *, cookies: Validator<NonNullable<Cookies>>}}
+     */
+    static propTypes = {
+        cookies: instanceOf(Cookies).isRequired,
+        friend: PropTypes.bool.isRequired,
+        uid: PropTypes.string.isRequired
+    };
+
+    /**
+     * This components will decide based on friend if the calling account can follow or unfollow.
+     * The uid is of the account shown to the calling account.
+     * @param props
+     */
     constructor(props) {
         super(props);
         this.state = {
             friend: this.props.friend,
             uid: this.props.uid,
         }
-
         this.handleClick = this.handleClick.bind(this)
     }
 
+    /**
+     * This method will be triggered if the button is clicked.
+     * Then it will follow or unfollow the corresponding account.
+     * This is done in the backend. Therefore the user token is needed.
+     * @param event when clicking the button.
+     */
     handleClick = (event) => {
         event.preventDefault();
         const {cookies} = this.props
         const utkn = cookies.get("utkn")
-        console.log(utkn)
         let url = 'http://192.168.0.3:8000/accounts/follow/'.concat(this.state.uid).concat("/")
         if (this.state.friend)
             url = 'http://192.168.0.3:8000/accounts/unfollow/'.concat(this.state.uid).concat("/")
@@ -30,7 +53,6 @@ class FollowUnfollowButton extends Component {
             }
         }).then((res) => {
             if (res.status === 200) {
-                console.log("Success")
                 this.setState({
                     friend: !this.state.friend
                 })
@@ -40,6 +62,10 @@ class FollowUnfollowButton extends Component {
         })
     }
 
+    /**
+     * Follow/Unfollow button to provide a social option.
+     * @returns {JSX.Element}
+     */
     render() {
         return (
             <Button inverted animated='fade' onClick={this.handleClick}>
