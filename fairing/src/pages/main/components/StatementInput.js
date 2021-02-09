2640df57fe296c12bb060ec54eb6c2b9fd84d82c
feger <marc.feger@uni-duesseldorf.de>
2021-02-09 23:23:25 +0100

    Add autocompletion for resizable textbox for StatementInput.js

--- fairing/src/pages/main/components/StatementInput.js
+++ fairing/src/pages/main/components/StatementInput.js
@@ -1,6 +1,16 @@
 import React, {Component} from 'react';
-import {Button, Form, Segment, Sticky} from "semantic-ui-react";
-import ContentEditable from "react-contenteditable";
+import {Segment, Sticky} from "semantic-ui-react";
+import ReactTextareaAutocomplete from "@webscopeio/react-textarea-autocomplete";
+import TextareaAutosize from "react-textarea-autosize";
+import emoji from "@jukben/emoji-search";
+//import "@webscopeio/react-textarea-autocomplete/style.css";
+import "../../../scss/Autocomplete.css"
+
+const Emoji = ({entity: {name, char}}) => <div>{`${name}: ${char}`}</div>;
+const User = ({entity: {name}}) => <div>{`${name}`}</div>;
+const Loading = () => <div>Wait</div>;
+const names = [{name: "Marc"}, {name: "Jan"}, {name: "Aylin"}, {name: "Rüdiger"}]
+const hashtags = [{name: "NoSmoking"}, {name: "Football"}, {name: "BurgerKing"}, {name: "NoCoffeeNoWorkee"}]
 
 
 class StatementInput extends Component {
@@ -17,6 +27,11 @@ class StatementInput extends Component {
         console.log(this.state.html)
     };
 
+    filterByValue = (array, string) => {
+        return array.filter(o =>
+            Object.keys(o).some(k => o[k].toLowerCase().includes(string.toLowerCase())));
+    }
+
     /**
      * This component is a sticky input for statements which can be made by the calling user.
      * The offset of 65 is used because the MenuBar of semantic ui has a magic height of 64.93333...px
@@ -27,26 +42,45 @@ class StatementInput extends Component {
             <Segment basic style={{padding: 0}}>
                 <Sticky offset={this.props.offset} context={this.props.context}>
                     <Segment>
-                        <Form reply>
-                            <Form.Input name="StatementInput"
-                                        rows={1}
-                                        control={ContentEditable}
-                                        html={this.state.html}
-                                        disabled={false}
-                                        onChange={this.handleChange}
-                                        style={{
-                                            outline: "none",
-                                            resize: "none",
-                                            overflow: "auto"
-                                        }}>
-                            </Form.Input>
-                            <Button
-                                content='Go'
-                                labelPosition='left'
-                                icon='paper plane'
-                                primary
-                            />
-                        </Form>
+                        <ReactTextareaAutocomplete
+                            movePopupAsYouType
+                            loadingComponent={Loading}
+                            textAreaComponent={
+                                TextareaAutosize
+                            }
+                            style={{resize: "none"}}
+                            minChar={1}
+                            trigger={{
+                                ":": {
+                                    dataProvider: token => {
+                                        return emoji(token)
+                                            .slice(0, 10)
+                                            .map(({name, char}) => ({name, char}));
+                                    },
+                                    component: Emoji,
+                                    output: (item, trigger) => item.char
+                                },
+                                "@": {
+                                    dataProvider: token => {
+                                        return this.filterByValue(names, token)
+                                    },
+                                    component: User,
+                                    output: (item, trigger) => {
+                                        console.log(this.value)
+                                        return "@" + item.name
+                                    }
+                                },
+                                "#": {
+                                    dataProvider: token => {
+                                        return this.filterByValue(hashtags, token)
+                                    },
+                                    component: User,
+                                    output: (item, trigger) => {
+                                        return "#" + item.name
+                                    }
+                                }
+                            }}
+                        />
                     </Segment>
                 </Sticky>
             </Segment>
