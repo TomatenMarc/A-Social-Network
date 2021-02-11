8694e67358faa9403d2ad4bf16109e71ddb60dea
feger <marc.feger@uni-duesseldorf.de>
2021-02-11 15:03:42 +0100

    Add a global template for the layout grid of contents

---
+++ fairing/src/components/StickyContentGrid.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import {Grid, Sticky} from "semantic-ui-react";
+import {PropTypes} from "prop-types";
+
+class StickyContentGrid extends Component {
+
+    /**
+     * This component is a template for the grid used to show content.
+     * The left and right element of this grid is sticky and can be used e.g. for recommendations or information.
+     * The center is not sticky but it can be added later for customization.
+     * If the middle element should be sticky then this element must use the same context as passed to this template.‚
+     * @type {{left: *, contextRef: *, center: *, menuOffset: *, right: *}}
+     */
+    static propTypes = {
+        menuOffset: PropTypes.number.isRequired,
+        left: PropTypes.node.isRequired,
+        center: PropTypes.node.isRequired,
+        right: PropTypes.node.isRequired,
+        contextRef: PropTypes.object.isRequired
+    };
+
+    /**
+     * This is the gird used for the content.
+     * The left and right columns are not shown on mobile.
+     * Todo: Make non-sticky stacked columns for mobile-view.
+     * @returns {JSX.Element}
+     */
+    render() {
+        return (
+            <Grid centered columns={3} stackable divided>
+
+                <Grid.Column only={"computer tablet"}>
+                    <Sticky context={this.props.contextRef} offset={this.props.menuOffset}>
+                        {this.props.left}
+                    </Sticky>
+                </Grid.Column>
+
+                <Grid.Column>
+                    {this.props.center}
+                </Grid.Column>
+
+                <Grid.Column only={"computer tablet"}>
+                    <Sticky context={this.props.contextRef} offset={this.props.menuOffset}>
+                        {this.props.right}
+                    </Sticky>
+                </Grid.Column>
+            </Grid>
+        );
+    }
+}
+
+export default StickyContentGrid;
--- fairing/src/pages/main/segments/ContentView.js
+++ fairing/src/pages/main/segments/ContentView.js
@@ -1,12 +1,31 @@
 import React, {Component, createRef} from 'react';
-import {Grid, Image, Segment, Sticky} from "semantic-ui-react";
+import {Image, Segment} from "semantic-ui-react";
 import StatementInput from "../components/StatementInput";
 import _ from "lodash";
+import StickyContentGrid from "../../../components/StickyContentGrid";
+import {PropTypes} from "prop-types";
+
 
 const Placeholder = () => <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png'/>
 
 class ContentView extends Component {
 
+    /**
+     * This component is the content view of the main page.
+     * Notice: The view must provide a context for the sticky elements in the grid.
+     * Also the middle element does not contain a stick element for customization purpose.
+     * @type {{menuOffset: *}}
+     */
+    static propTypes = {
+        menuOffset: PropTypes.number.isRequired
+    };
+
+    /**
+     * This component is for the content at the main page.
+     * The content grid contains sticky elements.
+     * Therefore a context must be provided.
+     * @param props
+     */
     constructor(props) {
         super(props);
         this.contextRef = createRef()
@@ -15,40 +34,41 @@ class ContentView extends Component {
     /**
      * This component is divided into three main columns.
      * The left and right columns can be used to show recommendations or other information.
+     * They are sticky.
      * The column in the middle can be used for communication and for showing the contents.
+     * The StatementInput contains a sticky element. Under this is the content.
+     * Because of the sticky input the content gets scrollable.
      * @returns {JSX.Element}
      */
     render() {
         return (
             <Segment vertical>
                 <div ref={this.contextRef}>
-                    <Grid centered columns={3} stackable divided>
-
-                        <Grid.Column only={"computer tablet"}>
-                            <Sticky context={this.contextRef} offset={this.props.menuOffset}>
-                                <Segment basic>
-                                    <Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
-                                </Segment>
-                            </Sticky>
-                        </Grid.Column>
-
-                        <Grid.Column>
-                            <StatementInput context={this.contextRef} offset={this.props.menuOffset}/>
-                            <Segment>
-                                {_.times(10, (i) => (
-                                    <Placeholder key={i}/>
-                                ))}
+                    <StickyContentGrid
+                        contextRef={this.contextRef}
+                        menuOffset={this.props.menuOffset}
+                        left={
+                            <Segment basic>
+                                <Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
                             </Segment>
-                        </Grid.Column>
-
-                        <Grid.Column only={"computer tablet"}>
-                            <Sticky context={this.contextRef} offset={this.props.menuOffset}>
-                                <Segment basic>
-                                    <Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
+                        }
+                        center={
+                            <div>
+                                <StatementInput context={this.contextRef} offset={this.props.menuOffset}/>
+                                <Segment>
+                                    {_.times(10, (i) => (
+                                        <Placeholder key={i}/>
+                                    ))}
                                 </Segment>
-                            </Sticky>
-                        </Grid.Column>
-                    </Grid>
+                            </div>
+                        }
+                        right={
+                            <Segment basic>
+                                <Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
+                            </Segment>
+                        }
+                    >
+                    </StickyContentGrid>
                 </div>
             </Segment>
         );
