f48e193316a97eec1a9422df242f1eccefbf99c6
feger <marc.feger@uni-duesseldorf.de>
2021-02-16 19:35:01 +0100

    Move StatementTemplate.js to own file

---
+++ fairing/src/pages/account/private/components/ContentView.js
@@ -0,0 +1,47 @@
+import React, {Component, createRef} from 'react';
+import {Segment} from "semantic-ui-react";
+import StickyContentGrid from "../../../../components/StickyContentGrid";
+import Contents from "../../segments/Contents";
+
+
+class ContentView extends Component {
+    /**
+     * This component is the content view for the private account page.
+     * This view has more options then the public account.
+     * @param props
+     */
+    constructor(props) {
+        super(props);
+        this.contextRef = createRef()
+    }
+
+    /**
+     * This component shows the statements of the calling private account.
+     * Todo: Add side information like follower and follows.
+     * @returns {JSX.Element}
+     */
+    render() {
+        return (
+            <Segment vertical style={{zIndex: 0}}>
+                <div ref={this.contextRef}>
+                    <StickyContentGrid
+                        contextRef={this.contextRef}
+                        menuOffset={this.props.menuOffset}
+                        left={
+                            <div/>
+                        }
+                        center={
+                            <Contents account={this.props.account}/>
+                        }
+                        right={
+                            <div/>
+                        }
+                    >
+                    </StickyContentGrid>
+                </div>
+            </Segment>
+        );
+    }
+}
+
+export default ContentView;
