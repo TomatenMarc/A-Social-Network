a288a735e02e4d2432fe542528e927a31eabcb51
feger <marc.feger@uni-duesseldorf.de>
2021-02-26 13:31:11 +0100

    Let the hashtag observation show statements

--- fairing/src/pages/topic/segments/Contents.js
+++ fairing/src/pages/topic/segments/Contents.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
 import EmptyContentInformation from "../../../components/EmptyContentInformation";
+import {Comment, Segment} from "semantic-ui-react";
+import {StatementTemplate} from "../../../components/input/StatementTemplate";
 
 class Contents extends Component {
     render() {
         if (this.props.results.data.length === 0)
             return <EmptyContentInformation/>
         return (
-            <div>
-                Hey!
-            </div>
+            <Segment basic>
+                <Comment.Group>
+                    {
+                        this.props.results.data.map((item, index) => {
+                            return <StatementTemplate
+                                isParent={false}
+                                key={index}
+                                name={item.author.user.username}
+                                image={process.env.REACT_APP_API_URL.concat(item.author.image)}
+                                item={item}/>
+                        })
+                    }
+                </Comment.Group>
+            </Segment>
         );
     }
 }
