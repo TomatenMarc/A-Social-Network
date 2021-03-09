a521a59c5862fd2e27cdbbaf5234d2e08350b566
feger <marc.feger@uni-duesseldorf.de>
2021-03-09 19:35:05 +0100

    Modify public_core.sh

--- public_core.sh
+++ public_core.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-mkdir pypi/sofame
-touch pypi/sofame/__init__.py
-cp -r solar/accounts solar/authentication solar/contents solar/media solar/search pypi/sofame
+#mkdir pypi/sofame
+touch pypi/__init__.py
+cp -r solar/accounts solar/authentication solar/contents solar/media solar/search pypi/
 cd pypi/
 python setup.py sdist -d package/
 twine check package/*
+twine upload --repository-url https://test.pypi.org/legacy/ --skip-existing --non-interactive -u ${PYPI_USER} -p ${PYPI_PWD} package/*
