a6b6a077e4a30d2ea998235486b032db38b49395
Marc Feger <marc.feger@uni-duesseldorf.de>
2021-07-16 11:50:24 +0200

    Remove twine

--- public_core.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#mkdir pypi/sofame
-touch pypi/__init__.py
-cp -r solar/accounts solar/authentication solar/contents solar/media solar/search pypi/
-cd pypi/
-python setup.py sdist -d package/
-twine check package/*
-twine upload --repository-url https://test.pypi.org/legacy/ --skip-existing --non-interactive -u ${PYPI_USER} -p ${PYPI_PWD} package/*
