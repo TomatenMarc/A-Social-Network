5a0fdda3ee08cdfccc16f63cac4e2a1afe17d075
AndreasBurbach <aburbach@compeon.de>
2021-12-08 17:06:46 +0100

    remove solar and add the_social_network

--- solar/solar/settings.py
+++ solar/solar/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import logging
 import os
 from pathlib import Path
 
@@ -42,7 +41,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
-    'core',
+    'the_social_network',
 ]
 
 MIDDLEWARE = [
